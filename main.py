51f8f8b8f6c94b77fbf73a68b2d7c608e41f6e36
calscoo <scoutscott13@gmail.com>
2020-11-12 18:28:24 -0600

    Basic scrapy impl

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from scrapy.cmdline import execute
 
+# The string to be executed in the command line
+exec_string = "scrapy crawl quotes -O quotes.json"
 
-print("Hello World")
+execute(exec_string.split())
